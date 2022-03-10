dab4bfea0a7fea6b0440b53f2b0862eff69cf75d
Feng Suri <suri@Fengs-iMac.local>
2022-03-10 08:22:30 -0800

    Export geomap in one year

---
+++ js/geomap.js
@@ -0,0 +1,162 @@
+class ChoroplethMap {
+
+    /**
+     * Class constructor with basic configuration
+     * @param {Object}
+     * @param {Array}
+     */
+    constructor(_config, _data) {
+      this.config = {
+        parentElement: _config.parentElement,
+        containerWidth: _config.containerWidth || 800,
+        containerHeight: _config.containerHeight || 600,
+        margin: _config.margin || {top: 0, right: 0, bottom: 0, left: 0},
+        tooltipPadding: 10,
+        legendBottom: 50,
+        legendLeft: 50,
+        legendRectHeight: 12, 
+        legendRectWidth: 150
+      }
+      this.data = _data;
+      this.initVis();
+    }
+    
+    /**
+     * We initialize scales/axes and append static elements, such as axis titles.
+     */
+    initVis() {
+      let vis = this;
+  
+      // Calculate inner chart size. Margin specifies the space around the actual chart.
+      vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+      vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+  
+      // Define size of SVG drawing area
+      vis.svg = d3.select(vis.config.parentElement).append('svg')
+          .attr('width', vis.config.containerWidth)
+          .attr('height', vis.config.containerHeight);
+  
+      // Append group element that will contain our actual chart 
+      // and position it according to the given margin config
+      vis.chart = vis.svg.append('g')
+          .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+  
+      // Initialize projection and path generator
+      vis.projection = d3.geoMercator();
+      vis.geoPath = d3.geoPath().projection(vis.projection);
+  
+      vis.colorScale = d3.scaleLinear()
+          .range(['#cfe2f2', '#0d306b'])
+          .interpolate(d3.interpolateHcl);
+  
+  
+      // Initialize gradient that we will later use for the legend
+      vis.linearGradient = vis.svg.append('defs').append('linearGradient')
+          .attr("id", "legend-gradient");
+  
+      // Append legend
+      vis.legend = vis.chart.append('g')
+          .attr('class', 'legend')
+          .attr('transform', `translate(${vis.config.legendLeft},${vis.height - vis.config.legendBottom})`);
+      
+      vis.legendRect = vis.legend.append('rect')
+          .attr('width', vis.config.legendRectWidth)
+          .attr('height', vis.config.legendRectHeight);
+  
+      vis.legendTitle = vis.legend.append('text')
+          .attr('class', 'legend-title')
+          .attr('dy', '.35em')
+          .attr('y', -10)
+          .text('Export value in one year')
+  
+      vis.updateVis();
+    }
+  
+    updateVis() {
+      let vis = this;
+  
+      const exportExtent = d3.extent(vis.data.features, d => d.properties.export_value);
+      //console.log(exportExtent);
+      // const importtExtent = d3.extent(vis.data.features, d => d.properties.import_value);
+      // const popDensityExtent = d3.extent(vis.data.objects.collection.geometries, d => d.properties.pop_density);
+      
+      // Update color scale
+      //vis.colorScale.domain(popDensityExtent);
+      vis.colorScale.domain(exportExtent);
+        
+      // Define begin and end of the color gradient (legend)
+      vis.legendStops = [
+        { color: '#cfe2f2', value: exportExtent[0], offset: 0},
+        { color: '#0d306b', value: exportExtent[1], offset: 100},
+      ];
+  
+      vis.renderVis();
+    }
+  
+  
+    renderVis() {
+      let vis = this;
+  
+      // Convert compressed TopoJSON to GeoJSON format
+      //const countries = topojson.feature(vis.data, vis.data.objects.collection)
+      const countries = vis.data;
+      // console.log(countries);
+      // console.log(countries.features[1].properties.export_value);
+  
+      // Defines the scale of the projection so that the geometry fits within the SVG area
+      vis.projection.fitSize([vis.width, vis.height], countries);
+  
+      // Append world map
+      const countryPath = vis.chart.selectAll('.country')
+          .data(countries.features)
+        .join('path')
+          .attr('class', 'country')
+          .attr('d', vis.geoPath)
+          .attr('fill', d => {
+            if (d.properties.export_value) {
+              //console.log(d.properties.export_value);
+              return vis.colorScale(d.properties.export_value);
+            } else {
+              return 'url(#lightstripe)';
+            }
+          });
+  
+      countryPath
+          .on('mousemove', (event,d) => {
+            const export_value = d.properties.export_value ? `Export value: <strong>${d.properties.export_value}</strong> ` : 'No data available'; 
+            d3.select('#tooltip')
+              .style('display', 'block')
+              .style('left', (event.pageX + vis.config.tooltipPadding) + 'px')   
+              .style('top', (event.pageY + vis.config.tooltipPadding) + 'px')
+              .html(`
+                <div class="tooltip-title">${d.properties.name}</div>
+                <div>${export_value }</div>
+              `);
+          })
+          .on('mouseleave', () => {
+            d3.select('#tooltip').style('display', 'none');
+          });
+  
+      // Add legend labels
+      vis.legend.selectAll('.legend-label')
+          .data(vis.legendStops)
+        .join('text')
+          .attr('class', 'legend-label')
+          .attr('text-anchor', 'middle')
+          .attr('dy', '.35em')
+          .attr('y', 20)
+          .attr('x', (d,index) => {
+            return index == 0 ? 0 : vis.config.legendRectWidth;
+          })
+          .text(d => Math.round(d.value * 10 ) / 10);
+  
+      // Update gradient for legend
+      vis.linearGradient.selectAll('stop')
+          .data(vis.legendStops)
+        .join('stop')
+          .attr('offset', d => d.offset)
+          .attr('stop-color', d => d.color);
+  
+      vis.legendRect.attr('fill', 'url(#legend-gradient)');
+    }
+  }
--- js/main.js
+++ js/main.js
@@ -13,6 +13,55 @@ d3.json('data/rollup_force_data.json').then(_data => {
   }, primaryPartners[selectedTime]);
 })
 
+/**
+ * Load TopoJSON data of the world and the data of the world wonders
+ */
+
+ Promise.all([
+  d3.json('data/africa.json'),
+  d3.csv('data/region_population_density.csv'),
+  d3.json('data/world.json'),
+  d3.csv('data/clean_country_partner_hsproductsection_year.csv'),
+]).then(data => {
+  const geoData = data[0];
+  const countryData = data[1];
+  let worldGeoData = data[2];
+  const countryPartnerData = data[3];
+
+  console.log(geoData);
+  console.log(worldGeoData);
+  console.log(geoData.objects.collection.geometries.length);
+  console.log(countryData.length);
+  console.log(worldGeoData.features.length);
+  console.log(countryPartnerData.length);
+
+
+  // Already geodata, not topodata
+  worldGeoData.features.forEach(d => {
+    for (let i = 0; i < countryPartnerData.length; i++) {
+      if(d.id == countryPartnerData[i].location_code) {
+        d.properties.export_value = +countryPartnerData[i].export_value;
+        d.properties.import_value = +countryPartnerData[i].import_value;
+      }
+    }
+  });
+
+  // Combine both datasets by adding the population density to the TopoJSON file
+  geoData.objects.collection.geometries.forEach(d => {
+    for (let i = 0; i < countryData.length; i++) {
+      if (d.properties.name == countryData[i].region) {
+        d.properties.pop_density = +countryData[i].pop_density;
+      }
+    }
+  });
+
+  const choroplethMap = new ChoroplethMap({ 
+    parentElement: '#geomap'
+  },  worldGeoData);
+})
+.catch(error => console.error(error));
+
+
 dispatcher.on('updateTime', s => {
   selectedTime = s;
   overview.data = primaryPartners[selectedTime];
